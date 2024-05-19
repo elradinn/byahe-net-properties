8eeec96cee861087448d032b113abf56d314e38e
elradinn <johnalfredalfonso.dev@gmail.com>
2024-05-19 09:27:31 +0800

    updated category

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,16 +1,33 @@
-'use client'
+"use client";
 
 import Container from "../Container";
 
-import { TbBeach, TbMountain, TbPool, TbUfo } from 'react-icons/tb';
-import { GiWindmill, GiIsland, GiCastle, GiForestCamp, GiMountainCave, GiCactus, GiBarn, GiJungle, GiTreehouse, GiFlowerPot, GiFamilyHouse } from 'react-icons/gi'
-import { MdOutlineCabin, MdOutlineDiamond, MdOutlineVilla } from 'react-icons/md'
-import { FaLandmark, FaSkiing } from 'react-icons/fa';
-import { BsSnow, BsWater } from 'react-icons/bs';
-import { HiOutlineHomeModern } from 'react-icons/hi2';
+import { TbBeach, TbMountain, TbPool, TbUfo, TbVolcano } from "react-icons/tb";
+import {
+    GiWindmill,
+    GiIsland,
+    GiCastle,
+    GiForestCamp,
+    GiMountainCave,
+    GiCactus,
+    GiBarn,
+    GiJungle,
+    GiTreehouse,
+    GiFlowerPot,
+    GiFamilyHouse,
+    GiPalmTree,
+} from "react-icons/gi";
+import {
+    MdOutlineCabin,
+    MdOutlineDiamond,
+    MdOutlineVilla,
+} from "react-icons/md";
+import { FaLandmark, FaSkiing, FaWater } from "react-icons/fa";
+import { BsSnow, BsWater } from "react-icons/bs";
+import { HiOutlineHomeModern } from "react-icons/hi2";
 import { AiOutlineHeart } from "react-icons/ai";
 
-import CategoryBox from '../CategoryBox';
+import CategoryBox from "../CategoryBox";
 import { useSearchParams, usePathname } from "next/navigation";
 import { IoMdSwitch } from "react-icons/io";
 import useSearchModal from "@/app/hooks/useSearchModal";
@@ -18,133 +35,155 @@ import useSearchModal from "@/app/hooks/useSearchModal";
 // import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
 // import { IoMdSwitch } from "react-icons/io";
 
-
 export const categories = [
     {
-        label: 'Beachfront',
+        label: "Beachfront",
         icon: TbBeach,
-        description: 'Enjoy a seaside escape with beautiful views and easy access to the beach.'
+        description:
+            "Enjoy a seaside escape with beautiful views and easy access to the beach.",
     },
     {
-        label: 'Rustic',
+        label: "Rustic",
         icon: GiWindmill,
-        description: 'Experience a simpler way of life with cozy cabins and homes that evoke a bygone era.'
+        description:
+            "Experience a simpler way of life with cozy cabins and homes that evoke a bygone era.",
     },
     {
-        label: 'Cabin',
+        label: "Cabin",
         icon: MdOutlineCabin,
-        description: 'Experience rustic living in a cozy cabin. Perfect for a romantic getaway or a peaceful retreat in nature.'
+        description:
+            "Experience rustic living in a cozy cabin. Perfect for a romantic getaway or a peaceful retreat in nature.",
     },
     {
-        label: 'Modern',
+        label: "Modern",
         icon: MdOutlineVilla,
-        description: 'Stay in sleek, contemporary homes that offer style and sophistication.'
+        description:
+            "Stay in sleek, contemporary homes that offer style and sophistication.",
     },
     {
-        label: 'Mountain',
+        label: "Mountain",
         icon: TbMountain,
-        description: 'Escape to the mountains and enjoy breathtaking views, fresh air, and outdoor activities.'
+        description:
+            "Escape to the mountains and enjoy breathtaking views, fresh air, and outdoor activities.",
     },
     {
-        label: 'Pools',
+        label: "Pools",
         icon: TbPool,
-        description: 'Relax and unwind in homes with private pools, hot tubs, and luxurious amenities.'
+        description:
+            "Relax and unwind in homes with private pools, hot tubs, and luxurious amenities.",
     },
     {
-        label: 'Tropical',
+        label: "Tropical",
         icon: GiIsland,
-        description: 'Discover our Tropical homes, a perfect escape to paradise. Relax and explore with stunning views, beaches, and exotic nature.'
+        description:
+            "Discover our Tropical homes, a perfect escape to paradise. Relax and explore with stunning views, beaches, and exotic nature.",
     },
     {
-        label: 'Lakefront',
+        label: "Lakefront",
         icon: BsWater,
-        description: 'Enjoy lakefront homes with beautiful views and water activities like fishing and boating.'
+        description:
+            "Enjoy lakefront homes with beautiful views and water activities like fishing and boating.",
     },
     {
-        label: "Tiny home",
+        label: "Tiny Home",
         icon: HiOutlineHomeModern,
-        description: "Experience the cozy and minimalist lifestyle in a compact home, often with unique and space-saving designs."
+        description:
+            "Experience the cozy and minimalist lifestyle in a compact home, often with unique and space-saving designs.",
     },
     {
         label: "OMG!",
         icon: TbUfo,
-        description: "Unleash your inner weirdo and stay in a place that's as unique as you are! From treehouses to underground bunkers, these listings are anything but ordinary."
-      },
-    {
-        label: 'Skiing',
-        icon: FaSkiing,
-        description: 'Hit the slopes and stay in cozy homes and cabins near popular ski resorts.'
+        description:
+            "Unleash your inner weirdo and stay in a place that's as unique as you are! From treehouses to underground bunkers, these listings are anything but ordinary.",
     },
     {
-        label: 'Castle',
-        icon: GiCastle,
-        description: 'Live like royalty in grand castles and chateaux with historic architecture and lavish interiors.'
+        label: "Island",
+        icon: GiPalmTree,
+        description:
+            "Stay on a private island and experience the ultimate in seclusion and luxury.",
     },
     {
-        label: 'Camping',
+        label: "Camping",
         icon: GiForestCamp,
-        description: 'Experience luxury camping in beautiful natural settings with all the amenities of home.'
+        description:
+            "Experience luxury camping in beautiful natural settings with all the amenities of home.",
     },
     {
-        label: 'Arctic',
-        icon: BsSnow,
-        description: 'Embark on an adventure to the icy north and see the stunning natural beauty of the Arctic.'
-    },
-    {
-        label: 'Caves',
+        label: "Caves",
         icon: GiMountainCave,
-        description: 'Stay in unique and unusual homes carved into cliffs and nestled in natural caves.'
+        description:
+            "Stay in unique and unusual homes carved into cliffs and nestled in natural caves.",
     },
     {
-        label: 'Desert',
-        icon: GiCactus,
-        description: 'Find peace and solitude in the vast deserts of the world, with unique and luxurious homes.'
-    },
-    {
-        label: 'Farm',
+        label: "Farm",
         icon: GiBarn,
-        description: 'Get back to nature with farm stays and rural escapes, surrounded by fields and countryside.'
+        description:
+            "Get back to nature with farm stays and rural escapes, surrounded by fields and countryside.",
     },
     {
         label: "Mansion",
         icon: GiFamilyHouse,
-        description: "Live like royalty in these grand and luxurious estates, complete with lavish amenities and sprawling grounds."
+        description:
+            "Live like royalty in these grand and luxurious estates, complete with lavish amenities and sprawling grounds.",
     },
     {
-        label: 'Jungle',
+        label: "Jungle",
         icon: GiJungle,
-        description: 'Explore lush forests and exotic wildlife in some of the world\'s most beautiful jungles.',
+        description:
+            "Explore lush forests and exotic wildlife in some of the world's most beautiful jungles.",
     },
     {
-        label: 'Romantic',
+        label: "Romantic",
         icon: AiOutlineHeart,
-        description: 'Experience the ultimate romantic getaway in some of the world\'s most beautiful destinations.',
+        description:
+            "Experience the ultimate romantic getaway in some of the world's most beautiful destinations.",
     },
     {
-        label: 'Garden',
+        label: "Garden",
         icon: GiFlowerPot,
-        description: 'Relax in beautiful gardens and green spaces.',
+        description: "Relax in beautiful gardens and green spaces.",
     },
     {
-        label: 'Historic',
+        label: "Historic",
         icon: FaLandmark,
-        description: 'Stay in some of the world\'s most famous historic sites and monuments.',
+        description:
+            "Stay in some of the world's most famous historic sites and monuments.",
     },
     {
-        label: 'Treehouse',
+        label: "Treehouse",
         icon: GiTreehouse,
-        description: 'Experience a unique and cozy getaway in a treehouse surrounded by nature.'
+        description:
+            "Experience a unique and cozy getaway in a treehouse surrounded by nature.",
     },
     {
-        label: 'Lux',
+        label: "Lux",
         icon: MdOutlineDiamond,
-        description: 'Indulge in opulent and extravagant homes and villas, with all the amenities of a five-star hotel.'
+        description:
+            "Indulge in opulent and extravagant homes and villas, with all the amenities of a five-star hotel.",
     },
-]
+    {
+        label: "Heritage",
+        icon: GiCastle,
+        description:
+            "Stay in homes that are part of the Philippines' rich cultural and historical heritage.",
+    },
+    {
+        label: "Surfing",
+        icon: FaWater,
+        description:
+            "Stay close to the best surfing spots and enjoy the waves in the Philippines.",
+    },
+    {
+        label: "Volcano View",
+        icon: TbVolcano,
+        description:
+            "Enjoy breathtaking views of the Philippines' famous volcanoes from your accommodation.",
+    },
+];
 
 const Categories = () => {
     const params = useSearchParams();
-    const category = params?.get('category');
+    const category = params?.get("category");
     const pathname = usePathname();
     const searchModal = useSearchModal();
 
@@ -169,36 +208,26 @@ const Categories = () => {
     //         });
     //     }
     // };
-    
 
-    const isMainPage = pathname === '/';
+    const isMainPage = pathname === "/";
 
     if (!isMainPage) return null;
 
     return (
         <Container categoryContainer>
-            <div 
+            <div
                 // ref={containerRef}
-                className="
-                    pt-2
-                    flex
-                    flex-row
-                    gap-4
-                    items-center
-                    justify-between
-                    overflow-x-auto
-                    w-full
-                "
+                className="flex flex-row items-center justify-between w-full gap-4 pt-2 overflow-x-auto "
             >
                 {/* {showRightScroll && (
                     <div className="w-[220px] h-1/2 flex flex-row items-center justify-end gap-4 absolute right-[80px] bg-gradient-to-r from-transparent from-10% via-white via-30% to-white to-90%">
                         <button
-                            className="bg-white border rounded-full shadow-md p-2 text-neutral-800"
+                            className="p-2 bg-white border rounded-full shadow-md text-neutral-800"
                             onClick={handleRightScroll}
                         >
                             <AiOutlineRight />
                         </button>
-                        <div className="text-neutral-800 px-3 py-4 border rounded-xl bg-white">
+                        <div className="px-3 py-4 bg-white border text-neutral-800 rounded-xl">
                             <div className="flex flex-row items-center gap-4">
                             <IoMdSwitch /> Filters
                             </div>
@@ -216,36 +245,17 @@ const Categories = () => {
                     />
                 ))}
             </div>
-            <div 
+            <div
                 onClick={searchModal.onOpen}
-                className="
-                        hidden
-                        sm:flex
-                        p-3 
-                        border 
-                        border-neutral-400 
-                        text-semibold 
-                        rounded-xl 
-                        flex-row 
-                        items-center 
-                        gap-2 
-                        font-bold 
-                        text-xs 
-                        cursor-pointer 
-                        hover:shadow-md
-                        transition
-                        ease-in-out
-                        duration-150
-                        ml-14
-                    "
-                >
-                <IoMdSwitch size={21}/> Filters
+                className="flex-row items-center hidden gap-2 p-3 text-xs font-bold transition duration-150 ease-in-out border cursor-pointer  sm:flex border-neutral-400 text-semibold rounded-xl hover:shadow-md ml-14"
+            >
+                <IoMdSwitch size={21} /> Filters
             </div>
-            {/* <div className="p-1 rounded-full border border-neutral-400 cursor-pointer">
+            {/* <div className="p-1 border rounded-full cursor-pointer border-neutral-400">
                 <FiChevronRight size={21}/>
             </div> */}
         </Container>
     );
-}
- 
+};
+
 export default Categories;
